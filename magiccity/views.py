1d1abc71c19847b5152424ba25de995aed236ddb
Jerry-Wemisiko <jerry.wemisiko@student.moringaschool.com>
2021-09-28 10:32:18 +0300

    new homepageview

--- magiccity/views.py
+++ magiccity/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
 from magiccity.models import Neighbourhood,Post,Business
-
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+@login_required(login_url='/accounts/login/')
+def homepage(request):
+    houses = Neighbourhood.objects.all()
+
+    return render(request, 'index.html',{'houses':houses})
+
