905083f971e62606dbef39011c77a712d29a85ca
Jerry-Wemisiko <jerry.wemisiko@student.moringaschool.com>
2021-09-28 10:34:43 +0300

    update urls.py

---
+++ magiccity/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from . import views
+from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
+from django.conf import settings
+
+urlpatterns =[
+
+    path(r'^$',views.index,name='index'),
+    path('register/',views.signup, name='registration'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/',auth_views.LogoutView.as_view(), name='logout'),
+ ]
+
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
