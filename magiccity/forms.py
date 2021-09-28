17b9504f487de651414a630621fa5fcded82597e
Jerry-Wemisiko <jerry.wemisiko@student.moringaschool.com>
2021-09-28 10:36:26 +0300

    new signup form

---
+++ magiccity/forms.py
@@ -0,0 +1,12 @@
+from magiccity.models import Business, Neighbourhood, Profile, Post
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+
+class SignupForm(UserCreationForm):
+    email = forms.EmailField(max_length=250))
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
--- magiccity/urls.py
+++ magiccity/urls.py
@@ -6,7 +6,7 @@ from django.conf import settings
 
 urlpatterns =[
 
-    path(r'^$',views.index,name='index'),
+    path('',views.index,name='index'),
     path('register/',views.signup, name='registration'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/',auth_views.LogoutView.as_view(), name='logout'),
