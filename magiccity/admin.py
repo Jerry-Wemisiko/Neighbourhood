63a69b1508fd53c0218659d7f1638663b7c3b3ce
Jerry-Wemisiko <jerry.wemisiko@student.moringaschool.com>
2021-09-28 11:13:23 +0300

    new supeuser owner created

--- magiccity/admin.py
+++ magiccity/admin.py
@@ -1,7 +1,9 @@
 from magiccity.models import Business, Neighbourhood, Post, Profile
 from django.contrib import admin
-
+from django.db import models
+from magiccity.models import Profile,Neighbourhood,Business,Post
 # Register your models here.
+
 admin.site.register(Profile)
 admin.site.register(Neighbourhood)
 admin.site.register(Post)
