c0faef69f69a8fceff4c69035f9cc6d58b4fdd2f
Jerry-Wemisiko <jerry.wemisiko@student.moringaschool.com>
2021-09-28 10:06:09 +0300

    new post model

--- magiccity/models.py
+++ magiccity/models.py
@@ -32,3 +32,29 @@ class Business(models.Model):
     def update_business(self):
         name = self.business_name
         self.business_name = name
+
+class Post(models.Model):
+    POST_CHOICES = (
+        ('1', 'Security'),
+        ('2', 'Health Emergency'),
+        ('3', 'Entertainment'),
+        ('4', 'Fire Breakouts'),
+        ('5', 'Playground'),
+        ('6', 'Death'),
+        ('7', 'Gym'),
+    )
+    category = models.CharField(max_length=120, choices=POST_CHOICES)
+    title = models.CharField(max_length=100, null=True)
+    post = models.TextField()
+    upload_date = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    neighbourhood = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.title} Post'    
+    
+    def save_post(self):
+        self.save()
+
+    def delete_post(self):
+        self.delete()
